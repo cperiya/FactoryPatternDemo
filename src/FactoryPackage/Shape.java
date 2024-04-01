a219baee723b5e0c76b935446999a2f1606bc95e
cperiya <157152157+cperiya@users.noreply.github.com>
2024-04-01 18:31:28 -0400

    Initial commit

---
+++ src/FactoryPackage/Shape.java
@@ -0,0 +1,5 @@
+package FactoryPackage;
+
+public interface Shape {
+	void draw();
+}
---
+++ src/FactoryPackage/Square.java
@@ -0,0 +1,8 @@
+package FactoryPackage;
+
+public class Square implements Shape{
+	@Override
+	public void draw() {
+		System.out.println("Inside square :: draw() method.");
+	}
+}
